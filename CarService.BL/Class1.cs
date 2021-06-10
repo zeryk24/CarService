dec92cda2e5872e943f56f907af87331bf8bb180
Erik <Erik@DESKTOP-NABF9MN>
2021-06-10 21:16:25 +0200

    Created projects

---
+++ CarService.BL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CarService.BL
+{
+    public class Class1
+    {
+    }
+}
---
+++ CarService.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CarService.Shared
+{
+    public class Class1
+    {
+    }
+}
