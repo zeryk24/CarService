641976606b8538c18b0b4a9fdd91720ad6307f86
Jan Skvaril <kondr4@seznam.cz>
2021-12-02 17:43:41 +0100

    Webapp - implemented search

--- CarService.WebApi/ClientApp/src/sites/Home.jsx
+++ CarService.WebApi/ClientApp/src/sites/Home.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Paper, Typography, Button, TextField, LinearProgress } from "@mui/material";
+import { Card, CardContent, Paper, Typography, Button, TextField, LinearProgress, Collapse } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import ChartCard from "../components/ChartCard";
 import LayoutContext from "../components/LayoutContext";
@@ -20,6 +20,11 @@ const MaterialCard = (props) => {
     </Card >
 }
 
+function IsSubstring(substring, string) {
+    substring = substring.toLowerCase();
+    string = string.toLowerCase();
+    return string.includes(substring);
+}
 
 const Home = (props) => {
     const fasade = new MaterialsFasade();
@@ -27,9 +32,14 @@ const Home = (props) => {
     const { navOpen, setNavOpen, siteName, setSiteName } = useContext(LayoutContext);
     const [oftenMaterials, setOftenMaterials] = useState([]);
     const [leastMaterials, setLeastMaterials] = useState([]);
+    const [oftenSeach, setOftenSeach] = useState("");
+    const [leastSeach, setLeastSeach] = useState("");
+    const [maxItems, setMaxItems] = useState(4);
+    const [colps, setColps] = useState(false);
     useEffect(() => {
         setSiteName("Analýza spotřeby materiálů")
         fasade.GetAll().then((data) => {
+            console.log(data);
             data = data.sort((a, b) => {
                 return a.consumes.length > b.consumes.length ? 1 : -1;
             });
@@ -39,22 +49,32 @@ const Home = (props) => {
                 return a.stockQuantity > b.stockQuantity ? 1 : -1;
             });
             setLeastMaterials(data);
-
+            setColps(true);
         })
     }, []);
     if (oftenMaterials.length == 0) {
-        return <LinearProgress />
+        return <div style={{ background: "#424242", height: "100%" }}>
+            <LinearProgress />
+        </div>
     }
 
     let often_material_cards = [];
     let k = 0;
     for (let mat of oftenMaterials) {
+        if (oftenSeach !== "" && !IsSubstring(oftenSeach, mat.name)) continue;
         often_material_cards.push(<MaterialCard key={k++} amount={mat.stockQuantity}>{mat.name}</MaterialCard>)
+        if (k == maxItems) {
+            break;
+        }
     }
     let least_material_cards = [];
     k = 0;
     for (let mat of leastMaterials) {
+        if (leastSeach !== "" && !IsSubstring(leastSeach, mat.name)) continue;
         least_material_cards.push(<MaterialCard key={k++} amount={mat.stockQuantity}>{mat.name}</MaterialCard>)
+        if (k == maxItems) {
+            break;
+        }
     }
     return <div
         style={{
@@ -67,22 +87,29 @@ const Home = (props) => {
             background: "#424242",
         }}>
         {/* Frequently used materials */}
-        <div style={{ minHeight: "105vh", display: "flex", justifyContent: "start", alignItems: "center", flexDirection: "column" }}>
-            <Typography color="primary" variant="h4">Často používané produkty</Typography>
-            <TextField sx={{ margin: "20px", width: "95%" }} label="Vyhledat" variant="outlined" />
-            {often_material_cards}
-            <Button sx={{ margin: "10px" }} variant="contained">Načíst více materiálů</Button>
+        <div style={{ minHeight: "155vh", display: "flex", justifyContent: "start", alignItems: "center", flexDirection: "column" }}>
+            <Typography color="primary" variant="h4" sx={{ width: "420px" }}>Často používané produkty</Typography>
+            <TextField value={oftenSeach} onChange={(e) => { setOftenSeach(e.target.value) }} sx={{ margin: "20px", width: "95%" }} label="Vyhledat" variant="outlined" />
+            <Collapse in={colps} timeout={500}>
+                {often_material_cards}
+            </Collapse>
+            {maxItems > often_material_cards.length ? "" :
+                <Button sx={{ margin: "10px" }} onClick={() => { setMaxItems(maxItems + 4) }} variant="contained">Načíst více materiálů</Button>
+            }
         </div>
-        <div style={{ display: "flex", flexGrow: "1", alignItems: "center", flexDirection: "column", paddingTop: "50px" }}>
+        <div style={{ overflow: "hidden", display: "flex", flexGrow: "1", alignItems: "center", flexDirection: "column", paddingTop: "50px" }}>
             <ChartCard />
         </div>
         {/* Missing materials */}
         <div style={{ minHeight: "105vh", display: "flex", justifyContent: "start", alignItems: "center", flexDirection: "column" }}>
-            <Typography color="primary" variant="h4">Nejméně na skladě</Typography>
-            <TextField sx={{ margin: "20px", width: "95%" }} label="Vyhledat" variant="outlined" />
-            {least_material_cards}
-            <Button sx={{ margin: "10px" }} variant="contained">Načíst více materiálů</Button>
-        </div>
+            <Typography color="primary" variant="h4" sx={{ width: "420px" }}>Nejméně na skladě</Typography>
+            <TextField value={leastSeach} onChange={(e) => { setLeastSeach(e.target.value) }} sx={{ margin: "20px", width: "95%" }} label="Vyhledat" variant="outlined" />
+            <Collapse in={colps} timeout={500}>
+                {least_material_cards}
+            </Collapse>
+            {maxItems > least_material_cards.length ? "" :
+                <Button sx={{ margin: "10px" }} onClick={() => { setMaxItems(maxItems + 4) }} variant="contained">Načíst více materiálů</Button>
+            }        </div>
     </div>
 }
 
